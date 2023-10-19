c496747aa555259646a5d20aac138e45feaebdd2
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-19 17:46:52 -0300

    feat: dashboard page created

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,12 @@
-export const Dashboard = () => {
+export const Dashboard = ({ user, userLogout }) => {
+  console.log(user);
   return (
     <main>
-      <h1>Página do usuário</h1>
+      {/* não sei se é p ou se vai no header isso a baixo e provavelmente não vai no main, não consegui encostar nessa página direito 
+      
+      OBS: o userLogout vai precisar ser mandando pro header caso o botão de sair vá no header ou caso ele fique aqui é só passar pro botão*/}
+      <p>{user?.name}</p>
+      <p>{user?.course_module}</p>
     </main>
   );
 };
