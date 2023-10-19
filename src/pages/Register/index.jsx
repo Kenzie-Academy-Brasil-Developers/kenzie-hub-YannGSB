954da5b6dc05448b77dcf805c540f9c2a74704ce
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-19 17:46:38 -0300

    feat: register page created

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,10 @@
+import { RegisterForm } from "../../components/forms/RegisterForm/index.jsx";
+
 export const Register = () => {
   return (
     <main>
       <h1>Página de registro</h1>
+      <RegisterForm />
     </main>
   );
 };
