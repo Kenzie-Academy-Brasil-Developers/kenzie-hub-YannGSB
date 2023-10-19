49edc785ffcc870ff992734bc0bd97b19342b889
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-19 20:39:01 -0300

    feat: imported toastify CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Routes from "./routes";
 import "./styles/index.scss";
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
