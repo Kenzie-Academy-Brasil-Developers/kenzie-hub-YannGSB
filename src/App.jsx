5b1a97064324336f4781b132e847beed08b966fc
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-19 17:47:47 -0300

    feat: toastify implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Routes from "./routes";
 import "./styles/index.scss";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   return (
     <>
       <Routes />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
