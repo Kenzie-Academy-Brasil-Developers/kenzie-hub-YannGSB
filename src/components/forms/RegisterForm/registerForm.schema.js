9e8a5fea126977bd3f6df8f2031ea4b9b19be754
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-19 17:47:11 -0300

    feat: register form schema created

---
+++ src/components/forms/RegisterForm/registerForm.schema.js
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const registerFormSchema = z
+  .object({
+    name: z.string().min(1, "O nome é obrigatório."),
+    email: z
+      .string()
+      .email("Forneça um e-mail válido.")
+      .min(1, "O e-mail é obrigatório."),
+    password: z
+      .string()
+      .min(8, "São necessários pelo menos oito caracteres.")
+      .regex(/([a-z]+)/, "É necessário conter pelo menos uma letra minuscula.")
+      .regex(/([A-Z]+)/, "É necessário conter pelo menos uma letra maiuscula.")
+      .regex(/([0-9]+)/, "É necessário pelo menos um número.")
+      .regex(
+        /([#?_!@$%^&*-]+)/,
+        "É necessário conter pelo menos um caractere especial"
+      ),
+    confirmPassword: z.string().min(1, "Por favor, confirme sua senha."),
+    bio: z
+      .string()
+      .min(4, "A bio precisa conter pelo menos quatro caracteres."),
+      contact: z.string(),
+      course_module: z.string(),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "As senhas não correspondem",
+    path: ["confirmPassword"],
+  });
