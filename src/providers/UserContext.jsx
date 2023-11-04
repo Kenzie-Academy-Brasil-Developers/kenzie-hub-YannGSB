a2c60948756f50b32a030eae11be04e6baddfeda
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-11-03 23:29:13 -0300

    feat: getUser created

--- src/providers/UserContext.jsx
+++ src/providers/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { api } from "../services/api.js";
@@ -8,6 +8,25 @@ export const UserContext = createContext({});
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const token = localStorage.getItem("@KENZIEHUBTOKEN");
+    const userId = localStorage.getItem("@KENZIEHUBUSERID");
+
+    const getUser = async () => {
+      try {
+        const { data } = await api.get(`/users/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUser(data);
+        navigate("/dashboard");
+      } catch (error) {}
+    };
+
+    getUser();
+  }, []);
+
   const navigate = useNavigate();
 
   const userLogout = () => {
@@ -15,6 +34,7 @@ export const UserProvider = ({ children }) => {
     navigate("/");
     toast.success("Logout realizado com sucesso");
     localStorage.removeItem("@KENZIEHUBTOKEN");
+    localStorage.removeItem("@KENZIEHUBUSERID");
   };
 
   const userLogin = async (payLoad, setLoading) => {
@@ -22,6 +42,7 @@ export const UserProvider = ({ children }) => {
       setLoading(true);
       const { data } = await api.post("/sessions", payLoad);
       localStorage.setItem("@KENZIEHUBTOKEN", data.token);
+      localStorage.setItem("@KENZIEHUBUSERID", data.user.id);
       setUser(data.user);
       toast.success("Login realizado com sucesso");
       navigate("/dashboard");
