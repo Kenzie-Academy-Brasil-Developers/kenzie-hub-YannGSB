e28b272ab371a0147daa5de73f40ce49e1e23353
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-11-07 23:42:17 -0300

    fix: import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import { Dashboard } from "./Dashboard";
 import { Login } from "./Login";
 import { Register } from "./Register";
 import { ErrorPage } from "./ErrorPage";
+import { LoadingPage } from "./LoadingPage";
 
-export { Dashboard, Login, Register, ErrorPage };
+export { Dashboard, Login, Register, ErrorPage, LoadingPage };
